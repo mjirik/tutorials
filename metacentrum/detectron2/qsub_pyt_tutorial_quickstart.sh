3fa952f16bda02d2bf1c7df5501c9d72b47dc412
Miroslav Jirik <mjirik@kky.zcu.cz>
2021-11-19 11:36:25 +0100

    detectron2

---
+++ metacentrum/detectron2/qsub_pyt_tutorial_quickstart.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=1:ngpus=1:mem=10gb:cl_konos=False:cl_gram=False:scratch_local=10gb
+#PBS -l walltime=01:00:00 -q gpu
+# modify/delete the above given guidelines according to your job's needs
+# Please note that only one select= argument is allowed at a time.
+
+# # PBS -l select=1:ncpus=1:mem=1gb:scratch_local=4gb
+
+# add to qsub with:
+# qsub qsub_pyt_tutorial_quickstart.sh
+
+# nastaveni domovskeho adresare, v promenne $LOGNAME je ulozeno vase prihlasovaci jmeno
+LOGDIR="/storage/plzen1/home/$LOGNAME/projects/tutorials/metacentrum/pytorch_quickstart/"
+PROJECTDIR="/storage/plzen1/home/$LOGNAME/projects/tutorials/metacentrum/detectron2/"
+#DATADIR="/storage/plzen4-ntis/projects/queetech/medical/processed/scaffan2019/metacentrum"
+
+
+echo "job: $PBS_JOBID running on: `uname -n`"
+
+
+# nastaveni automatickeho vymazani adresare SCRATCH pro pripad chyby pri behu ulohy
+trap 'clean_scratch' TERM EXIT
+
+# vstup do adresare SCRATCH, nebo v pripade neuspechu ukonceni s chybovou hodnotou rovnou 1
+cd $SCRATCHDIR || exit 1
+
+# priprava vstupnich dat (kopirovani dat na vypocetni uzel)
+# cp $DATADIR/gaussian_test.com $SCRATCHDIR
+
+# spusteni aplikace - samotny vypocet
+
+# activate environment option 1: miniconda installed
+module add cuda-10.1
+module add conda-modules-py37
+module add gcc-8.3.0
+
+#export PATH=/storage/plzen1/home/$LOGNAME/miniconda3/bin:$PATH
+#source activate mytorch
+conda activate drawnUI-conda
+conda activate /storage/brno3-cerit/home/$LOGNAME/.conda/envs/drawnUI-conda
+
+
+# this is because of python click
+# export LC_ALL=C.UTF-8
+# export LANG=C.UTF-8
+
+
+# Put your code here
+python  $PROJECTDIR/pyt_tutorial_quickstart.py > results.txt
+
+ls
+# kopirovani vystupnich dat z vypocetnicho uzlu do domovskeho adresare,
+# pokud by pri kopirovani doslo k chybe, nebude adresar SCRATCH vymazan pro moznost rucniho vyzvednuti dat
+cp results.txt $LOGDIR || export CLEAN_SCRATCH=false
+# cp *.jpg $LOGDIR || export CLEAN_SCRATCH=false
