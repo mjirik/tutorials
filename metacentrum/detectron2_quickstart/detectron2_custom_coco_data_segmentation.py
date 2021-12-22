287fc8f57db4db28a12f8f3898e7d6235489d02d
Miroslav Jirik <miroslav.jirik@gmail.com>
2021-12-22 23:40:21 +0100

    fix config path

--- metacentrum/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
+++ metacentrum/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
@@ -53,7 +53,7 @@ from detectron2.config import get_cfg
 import os
 
 cfg = get_cfg()
-cfg.merge_from_file(f"/storage/plzen1/logname/projects/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.merge_from_file(f"/storage/plzen1/home/{logname}/projects/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.DATASETS.TRAIN = ("fruits_nuts",)
 cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
 cfg.DATALOADER.NUM_WORKERS = 2
