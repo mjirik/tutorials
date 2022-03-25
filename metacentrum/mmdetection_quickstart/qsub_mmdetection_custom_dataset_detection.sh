387a663e4ddab21172ea93725783c7b5ee492b6d
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-03-25 19:19:03 +0100

    run with singularity

--- metacentrum/mmdetection_quickstart/qsub_mmdetection_custom_dataset_detection.sh
+++ metacentrum/mmdetection_quickstart/qsub_mmdetection_custom_dataset_detection.sh
@@ -74,7 +74,7 @@ export LANG=C.UTF-8
 
 
 # Put your code here
-python  $PROJECTDIR/mmdetection_custom_dataset_detection.py > results.txt
+singularity exec /auto/plzen4-ntis/projects/cv/CarnivoreID/carnivore_id\:v1.0.sif python  $PROJECTDIR/mmdetection_custom_dataset_detection.py > results.txt
 
 # kopirovani vystupnich dat z vypocetnicho uzlu do domovskeho adresare,
 # pokud by pri kopirovani doslo k chybe, nebude adresar SCRATCH vymazan pro moznost rucniho vyzvednuti dat
